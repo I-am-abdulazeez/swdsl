91cafd8c11e0296af339786f79a077a144921ad4
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-02-27 22:12:09 +0100

    Moved Login and Register Page to the pages folder

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,5 +1,42 @@
-import Index from "@components/pages/Auth/Register";
+import { VStack } from "@chakra-ui/layout";
 
-export default function Register() {
-  return <Index />;
-}
+import Helmet from "@components/Helmet";
+import AuthHeading from "@components/AuthHeading";
+import RegisterForm from "@components/pages/Auth/Register/RegisterForm";
+
+import { withPublic } from "src/hooks/useRoutes";
+import { AuthContextType } from "src/interfaces";
+
+const Index: React.FC<{ userAuth: AuthContextType }> = ({
+  userAuth,
+}): JSX.Element => {
+  const { isLoading, setUser, setIsLoggedIn, setIsLoading } = userAuth;
+
+  return (
+    <>
+      <Helmet title="Sign up | Create an Account | ShayoWithDSL.com" />
+      <VStack
+        as="section"
+        align="center"
+        justify="center"
+        h="100vh"
+        spacing={6}
+      >
+        <AuthHeading
+          authHeading="Create an account"
+          authText="Already have an account?"
+          authHref="/auth/login"
+          authRoute="Signin"
+        />
+        <RegisterForm
+          isLoading={isLoading}
+          setUser={setUser}
+          setIsLoading={setIsLoading}
+          setIsLoggedIn={setIsLoggedIn}
+        />
+      </VStack>
+    </>
+  );
+};
+
+export default withPublic(Index);
