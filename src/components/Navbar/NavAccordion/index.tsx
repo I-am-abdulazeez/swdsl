12515b67b0ef489bd25ef81300b275641fee18c0
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-05-03 15:15:30 +0100

    Created NavAccordion Component for simplicity

--- src/components/Navbar/NavAccordion/index.tsx
+++ src/components/Navbar/NavAccordion/index.tsx
@@ -1,4 +1,5 @@
 import { Accordion, AccordionItem, AccordionPanel } from "@chakra-ui/react";
+import { DrinkCategory } from "@data/index";
 
 import NavAccordionButton from "../NavAccordionButton";
 import NavCategoryList from "../NavCategoryList";
@@ -6,90 +7,14 @@ import NavCategoryList from "../NavCategoryList";
 const NavAccordion: React.FC = () => {
   return (
     <Accordion allowToggle>
-      <AccordionItem>
-        <NavAccordionButton>Non Alcoholic</NavAccordionButton>
-        <AccordionPanel>
-          <NavCategoryList category="Non alcoholic" />
-        </AccordionPanel>
-      </AccordionItem>
-      <AccordionItem>
-        <NavAccordionButton>Whiskey</NavAccordionButton>
-        <AccordionPanel>
-          <NavCategoryList category="Whiskey" />
-        </AccordionPanel>
-      </AccordionItem>
-      <AccordionItem>
-        <NavAccordionButton>Cognac</NavAccordionButton>
-        <AccordionPanel>
-          <NavCategoryList category="Cognac" />
-        </AccordionPanel>
-      </AccordionItem>
-      <AccordionItem>
-        <NavAccordionButton>Red wine</NavAccordionButton>
-        <AccordionPanel>
-          <NavCategoryList category="Red wine" />
-        </AccordionPanel>
-      </AccordionItem>
-      <AccordionItem>
-        <NavAccordionButton>Rum</NavAccordionButton>
-        <AccordionPanel>
-          <NavCategoryList category="Rum" />
-        </AccordionPanel>
-      </AccordionItem>
-      <AccordionItem>
-        <NavAccordionButton>Irish Cream</NavAccordionButton>
-        <AccordionPanel>
-          <NavCategoryList category="Irish cream" />
-        </AccordionPanel>
-      </AccordionItem>
-      <AccordionItem>
-        <NavAccordionButton>White Wine</NavAccordionButton>
-        <AccordionPanel>
-          <NavCategoryList category="White wine" />
-        </AccordionPanel>
-      </AccordionItem>
-      <AccordionItem>
-        <NavAccordionButton>Gin</NavAccordionButton>
-        <AccordionPanel>
-          <NavCategoryList category="Gin" />
-        </AccordionPanel>
-      </AccordionItem>
-      <AccordionItem>
-        <NavAccordionButton>Sparkling Wine</NavAccordionButton>
-        <AccordionPanel>
-          <NavCategoryList category="Sparkling wine" />
-        </AccordionPanel>
-      </AccordionItem>
-      <AccordionItem>
-        <NavAccordionButton>Brandy</NavAccordionButton>
-        <AccordionPanel>
-          <NavCategoryList category="Brandy" />
-        </AccordionPanel>
-      </AccordionItem>
-      <AccordionItem>
-        <NavAccordionButton>Juice</NavAccordionButton>
-        <AccordionPanel>
-          <NavCategoryList category="Juice" />
-        </AccordionPanel>
-      </AccordionItem>
-      <AccordionItem>
-        <NavAccordionButton>Tequila</NavAccordionButton>
-        <AccordionPanel>
-          <NavCategoryList category="Tequila" />
-        </AccordionPanel>
-      </AccordionItem>
-      <AccordionItem>
-        <NavAccordionButton>Vodka</NavAccordionButton>
-        <AccordionPanel>
-          <NavCategoryList category="Vodka" />
-        </AccordionPanel>
-      </AccordionItem>
-      <AccordionItem>
-        <NavAccordionButton>Others</NavAccordionButton>
-        <AccordionPanel>
-          <NavCategoryList category="Others" />
-        </AccordionPanel>
-      </AccordionItem>
+      {DrinkCategory.map((cat) => (
+        <AccordionItem>
+          <NavAccordionButton>{cat}</NavAccordionButton>
+          <AccordionPanel>
+            <NavCategoryList category={cat} />
+          </AccordionPanel>
+        </AccordionItem>
+      ))}
     </Accordion>
   );
 };
