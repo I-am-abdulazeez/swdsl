418c0d8b47e418b977a973d0eed79c0bd8ebeefd
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-05-07 08:27:16 +0100

    Profile page created

---
+++ src/pages/profile.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@chakra-ui/react";
+
+const Index: React.FC = () => {
+  return (
+    <Box>
+      <Text>Profile Page</Text>
+    </Box>
+  );
+};
+
+export default Index;
