d238587448abd4eff874729d6284a333a5efa54e
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-05-13 00:04:25 +0100

    feat: created withPrivate Route for authenticated users

--- src/hooks/useRoute.tsx
+++ src/hooks/useRoute.tsx
@@ -24,3 +24,24 @@ export const withPublic = (Component: React.FC<UserAuthType>) => {
     return <Component userAuth={auth} {...props} />;
   };
 };
+
+export const withPrivate = (Component: React.FC<UserAuthType>) => {
+  return function WithPrivate(props: AuthContextType) {
+    const auth = useAuth();
+    const { replace } = useRouter();
+
+    if (!auth.user) {
+      setTimeout(() => {
+        replace("/auth/login");
+      }, 2000);
+
+      return (
+        <Flex align={"center"} h={"100vh"} justify={"center"}>
+          <Spinner color="primary.600" />
+        </Flex>
+      );
+    }
+
+    return <Component userAuth={auth} {...props} />;
+  };
+};
