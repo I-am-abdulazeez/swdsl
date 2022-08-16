9fce82f45e64086638ca04499d2d0d4b190e7ceb
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-16 10:05:00 +0100

    chore: store from zustand to  handleAuthState change

--- src/components/ClientOnly/index.tsx
+++ src/components/ClientOnly/index.tsx
@@ -1,31 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { PropsWithChildren, useEffect, useState } from 'react';
 
-import { useAuth } from "@hooks/useAuth";
-
-import { ReactChildrenProp } from "@interfaces/index";
-
-import { firebaseAuth } from "@config/firebase";
-
-const ClientOnly: React.FC<ReactChildrenProp> = (props) => {
+const ClientOnly: React.FC<PropsWithChildren<{}>> = (props) => {
   const { children, ...delegated } = props;
 
   const [hasMounted, setHasMounted] = useState<boolean>(false);
-  const { setUser } = useAuth();
 
   useEffect(() => {
-    const unsubscribe = firebaseAuth.onAuthStateChanged((user) => {
-      if (user) {
-        setUser(user);
-        setHasMounted(true);
-        console.log(user);
-      } else {
-        setUser(null);
-        setHasMounted(true);
-      }
-    });
-    // Cleanup subscription on unmount
-    return () => unsubscribe();
-  }, [setUser]);
+    setHasMounted(true);
+  }, []);
 
   if (!hasMounted) return null;
 
