9c1fbe2c627d6007a2a9e38a020281a2b4514068
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-16 08:08:28 +0100

    chore: useAuthStore hook to access auth values

---
+++ src/store/hooks/useAuthStore/index.ts
@@ -0,0 +1,23 @@
+import create from 'zustand';
+import { persist, devtools } from 'zustand/middleware';
+
+import { authActions } from '@store/actions/auth-actions';
+import { AuthState, AuthStore } from '@store/types';
+
+const initialState: AuthState = {
+  user: null,
+  isLoading: false,
+  isAuthenticated: false,
+};
+
+export const useAuthStore = create<AuthStore>()(
+  devtools(
+    persist(
+      () => ({
+        ...initialState,
+        ...authActions,
+      }),
+      { name: 'authState' }
+    )
+  )
+);
