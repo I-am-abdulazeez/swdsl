0f923951552a4eef03982a49d20c1537e8accc1a
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-09-12 11:33:01 +0100

    chore: types for app

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,49 @@
 import { Timestamp } from 'firebase/firestore';
 
+export type UserDetails = {
+  email: string;
+  password: string;
+};
+
+export type IUserRegister = {
+  firstname: string;
+  lastname: string;
+  phonenumber: string;
+  accept: boolean;
+} & UserDetails;
+
+export type UserPasswordUpdate = {
+  newPassword: string;
+  confirmNewPassword: string;
+};
+
+export type Cart = {
+  id: string | string[] | undefined;
+  url: string;
+  drinkName: string;
+  price: number;
+  qty?: number;
+  category: string;
+  description?: string;
+};
+
 export type Product = {
+  productId: string;
   drinkName: string;
   description: string;
   category: string;
-  price: Number | string;
+  price: number;
   url: string;
   createdAt: Timestamp | null;
+};
+
+export type UserData = {
+  email: string;
+  firstname: string;
+  lastname: string;
+  phonenumber: string;
+  accept: boolean;
+  createdAt: Timestamp | string;
+  cart?: Cart[];
+  orders?: any[];
 };
