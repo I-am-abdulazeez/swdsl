21417e08ef39d53d7ef078fc1040dd705194c4d6
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-09-12 11:47:52 +0100

    chore: store-state types

--- src/store/types/index.ts
+++ src/store/types/index.ts
@@ -1,11 +1,15 @@
 import { User } from 'firebase/auth';
+import { DocumentData } from 'firebase/firestore';
 
-import { IUserRegister, UserData, UserDetails } from '@interfaces/index';
+import { IUserRegister, UserDetails, UserData, Cart, Product } from 'src/types';
+
+// Auth Store
 
 export type AuthState = {
   user: User | null;
   isLoading: boolean | undefined;
   isAuthenticated: boolean;
+  // isLoadingPassword: boolean | undefined;
 };
 
 export type AuthActions = {
@@ -13,7 +17,40 @@ export type AuthActions = {
   signInUser: (user: UserDetails) => void;
   forgotPassword: (email: string) => void;
   resetPassword: (oobCode: string, password: string) => void;
+  updateDisplayName: (displayName: string) => void;
+  // updatePassword: (newPassword: string) => void;
   signOutUser: () => void;
 };
 
 export type AuthStore = AuthState & AuthActions;
+
+// Product Store
+
+export type ProductState = {
+  products: DocumentData[] | null | [];
+  product: DocumentData | null;
+  isLoading: boolean | undefined;
+  isLoadingError: boolean | undefined;
+};
+
+export type ProductActions = {
+  getProducts: () => void;
+  getProduct: (productId: string) => void;
+};
+
+export type ProductStore = ProductState & ProductActions;
+
+// Cart Store
+
+export type CartState = {
+  cart: Cart[];
+  isLoading: boolean;
+};
+
+export type CartActions = {
+  addProduct: (cartItem: Cart) => void;
+  removeProduct: (cartItem: Cart) => void;
+  removeAllProduct: (cartItems: Cart) => void;
+};
+
+export type CartStore = CartState & CartActions;
