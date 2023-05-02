6b846d384d7bcfe898aec2b85e3fbcf3fb683dbd
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-05-02 11:01:46 +0100

    chore: restructure NavAccordion to display category as link

--- src/components/Navbar/NavAccordion/index.tsx
+++ src/components/Navbar/NavAccordion/index.tsx
@@ -1,4 +1,11 @@
-import { Accordion, AccordionItem, AccordionPanel } from '@chakra-ui/react';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Button,
+} from '@chakra-ui/react';
+import NextLink from 'next/link';
 import { DrinkCategory } from '@data/index';
 
 import NavAccordionButton from '../NavAccordionButton';
@@ -6,16 +13,38 @@ import NavCategoryList from '../NavCategoryList';
 
 const NavAccordion: React.FC = () => {
   return (
-    <Accordion allowToggle>
+    <>
       {DrinkCategory.map((category, idx) => (
-        <AccordionItem key={idx}>
-          <NavAccordionButton category={category} />
-          <AccordionPanel>
-            <NavCategoryList category={category} />
-          </AccordionPanel>
-        </AccordionItem>
+        <Box
+          borderBottom={'1px solid #f2f2f2'}
+          textAlign={'left'}
+          key={idx}
+          p={2}
+        >
+          <NextLink href={`/category/${category}`} passHref>
+            <Button
+              justifyContent={'left'}
+              width={'full'}
+              variant={'ghost'}
+              fontWeight={'medium'}
+            >
+              {category}
+            </Button>
+          </NextLink>
+        </Box>
       ))}
-    </Accordion>
+    </>
+
+    // <Accordion allowToggle>
+    //   {DrinkCategory.map((category, idx) => (
+    //     <AccordionItem key={idx}>
+    //       <NavAccordionButton category={category} />
+    //       <AccordionPanel>
+    //         <NavCategoryList category={category} />
+    //       </AccordionPanel>
+    //     </AccordionItem>
+    //   ))}
+    // </Accordion>
   );
 };
 
