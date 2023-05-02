fbcc31519f7773a766ece6a3872c956b8e684ac5
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-05-02 11:03:13 +0100

    chore: prettier in action

--- src/components/Products/ProductBadge/index.tsx
+++ src/components/Products/ProductBadge/index.tsx
@@ -1,5 +1,5 @@
-import { Badge } from "@chakra-ui/layout";
-import { DocumentData } from "firebase/firestore";
+import { Badge } from '@chakra-ui/layout';
+import { DocumentData } from 'firebase/firestore';
 
 const ProductBadge: React.FC<DocumentData> = (props) => {
   const { product } = props;
@@ -7,40 +7,38 @@ const ProductBadge: React.FC<DocumentData> = (props) => {
   return (
     <Badge
       borderRadius="md"
-      fontSize={"0.7em"}
-      fontWeight={"semibold"}
+      fontSize={'0.7em'}
+      fontWeight={'semibold'}
       variant="subtle"
       colorScheme={
-        product?.category === "Cognac"
-          ? "whatsapp"
-          : product?.category === "Juice"
-          ? "orange"
-          : product?.category === "Whiskey"
-          ? "purple"
-          : product?.category === "Non alcoholic"
-          ? "teal"
-          : product?.category === "Red wine"
-          ? "red"
-          : product?.category === "Champagne"
-          ? "cyan"
-          : product?.category === "Rum"
-          ? "yellow"
-          : product?.category === "Irish cream"
-          ? "twitter"
-          : product?.category === "White wine"
-          ? "gray"
-          : product?.category === "Gin"
-          ? "green"
-          : product?.category === "Sparkling wine"
-          ? "primary"
-          : product?.category === "Brandy"
-          ? "secondary"
-          : product?.category === "Tequila"
-          ? "messenger"
-          : product?.category === "Vodka"
-          ? "blackAlpha"
-          : product?.category === "Others"
-          ? "telegram"
+        product?.category === 'Cognac'
+          ? 'whatsapp'
+          : product?.category === 'Juice'
+          ? 'orange'
+          : product?.category === 'Whiskey'
+          ? 'purple'
+          : product?.category === 'Red wine'
+          ? 'red'
+          : product?.category === 'Champagne'
+          ? 'cyan'
+          : product?.category === 'Rum'
+          ? 'yellow'
+          : product?.category === 'Irish cream'
+          ? 'twitter'
+          : product?.category === 'White wine'
+          ? 'gray'
+          : product?.category === 'Gin'
+          ? 'green'
+          : product?.category === 'Sparkling wine'
+          ? 'primary'
+          : product?.category === 'Brandy'
+          ? 'secondary'
+          : product?.category === 'Tequila'
+          ? 'messenger'
+          : product?.category === 'Vodka'
+          ? 'pink'
+          : product?.category === 'Others'
+          ? 'teal'
           : undefined
       }
     >
