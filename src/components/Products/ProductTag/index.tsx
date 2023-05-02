236a7fed55546bd91285b3782b70a51be9a7f6d1
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-05-02 11:03:51 +0100

    refactor: ProductTag component, chnage colorScheme dynamically

--- src/components/Products/ProductTag/index.tsx
+++ src/components/Products/ProductTag/index.tsx
@@ -9,8 +9,19 @@ const ProductTag: React.FC<ProductProps> = ({ product }) => {
           fontWeight={'semibold'}
           fontSize={'12px'}
           ml={2}
+          variant={'outline'}
           colorScheme={
-            product?.packSize === '12 Packs' ? 'primary' : 'secondary'
+            product?.packSize === '3 Packs'
+              ? 'primary'
+              : product?.packSize === '4 Packs'
+              ? 'red'
+              : product?.packSize === '6 Packs'
+              ? 'pink'
+              : product?.packSize === '10 Packs'
+              ? 'orange'
+              : product?.packSize === '12 Packs'
+              ? 'yellow'
+              : undefined
           }
         >
           {product?.packSize}
